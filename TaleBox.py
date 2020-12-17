7ff2edaa592180acd3d4527ae909862961ed2469
yasmine <yasmine.ben-fredj@etu.unice.fr>
2020-12-17 16:54:46 +0100

    Avoir une version du jeu qui va permettre d'analyser une histoire et donner son genre (thème) #1

--- TaleBox.py
+++ TaleBox.py
@@ -10,54 +10,31 @@ from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 import gensim.models 
 from nltk.corpus import stopwords
-
+from sklearn.feature_extraction.text import CountVectorizer
 import spacy
-nlp = spacy.load("fr_core_news_md")
-
 from nltk.stem import WordNetLemmatizer
+from sklearn.decomposition import LatentDirichletAllocation as LDA
+from sklearn.model_selection import GridSearchCV
+
+nlp = spacy.load("fr_core_news_md")
 lemmatizer = WordNetLemmatizer()
 
+"""   1 : Import Dataset   """
 
-
-#hardCoded repense :
-#from nltk.chat.util import Chat, reflections
-##hardCode = [
-##    ['(Bonjour | Coucou | Salut )' , ['Salut toi ! ']],
-##    ["Comment tu t'appelles ?" , ["Je m'appelle TaleBox"]],
-##    ["(Qui est tu ?|Tu sert à quoi ?|Tu sais faire quoi?)", ["Je suis conçu pour crée des contes avec ton aide.", 'Je suis un bot qui crée des contes.' , "Je m'appelle TaleBox et je suis la pour crée un conte avec toi." ]],
-##    ["(Commençant|commencer|Je veux commencer|Comment Commencer?)", ["Tu veux qu'on commence notre histoire?", "Pour commencer, tu peut me donner un lieu ?"]]] 
-##
-##
-##chat = Chat(hardCode , reflections)
-##chat.converse()
-
-
-
-
-class Repondre_Prediction:
-    message =''
-    
-    def __init__(self,message):
-        self.message = message
-
-
-
-
-
-############### 1 : Import Dataset
-
-data_contes = pd.read_csv(r"C:\Users\Jasmine\Desktop\Master 1\Semestre 1\TATIA\Projet\TaleBox\TaleBox_contes.csv", sep=',', encoding ='ISO-8859-1')
-data_genres = pd.read_csv(r"C:\Users\Jasmine\Desktop\Master 1\Semestre 1\TATIA\Projet\TaleBox\TaleBox_genres.csv", sep=',', encoding ='ISO-8859-1')
-
+data_contes = pd.read_csv(r"TaleBox_contes.csv", sep=',', encoding ='ISO-8859-1')
+data_genres = pd.read_csv(r"TaleBox_genres.csv", sep=',', encoding ='ISO-8859-1')
 contes = data_contes['conte'].tolist()
 genres = data_contes['genre'].tolist()
-############### 2 : retirer caractére spéciaux 
 
-# Remove new line characters
-contes= [re.sub('\s+', ' ', sent) for sent in contes]
+"""   2 : retirer caractére spéciaux   """
+
+def sup_caractere_spéciaux(contes):
+    return [re.sub('\s+', ' ', sent) for sent in contes]
+ 
+contes = sup_caractere_spéciaux(contes)
 
 
-############### 3 : Tokenisation 
+"""   3 : Tokenisation  """
 
 def tokenize(sentences) :
     for mot in sentences:
@@ -65,48 +42,49 @@ def tokenize(sentences) :
 
 tokens = list(tokenize(contes))
 
-############## 4 : creation de bigram et trigram français
+"""  4 : creation de bigram et trigram français  """
+""" seuil (threshold)  plus élevé ==> moins de phrases.
+avec 10 on se retourve avec resultat faut (ex: la_maison ) 
+avec 100 on a toujours une faute (ex: tu_es)
+donc on laisse le threshold a 150 """
 
-bigram =  gensim.models.Phrases(tokens, min_count=5, threshold=150)
-trigram =  gensim.models.Phrases(bigram[tokens], threshold=150)  
+def get_bigrams(tokens):
+    bigram =  gensim.models.Phrases(tokens, min_count=5, threshold=150)
+    bigram_model = gensim.models.phrases.Phraser(bigram)
 
-# seuil ( threshold)  plus élevé ==> moins de phrases.
-#avec 10 on se retourve avec resultat faut (ex: la_maison ) ,
-#avec 100 on a toujours une faute comme "tu_es"
-#donc on laisse le threshold a 150
-
-
-bigram_model = gensim.models.phrases.Phraser(bigram)
-trigram_model = gensim.models.phrases.Phraser(trigram)
-
-def get_bigrams(data):
-    for conte in data :
+    for conte in tokens :
         yield(bigram_model[conte])
 
-def get_trigrams(data):
-    for conte in data :
+def get_trigrams(tokens):
+    bigram =  gensim.models.Phrases(tokens, min_count=5, threshold=150)
+    trigram =  gensim.models.Phrases(bigram[tokens], threshold=150)
+    bigram_model = gensim.models.phrases.Phraser(bigram)
+    trigram_model = gensim.models.phrases.Phraser(trigram)
+
+    for conte in tokens :
         yield(trigram_model[bigram_model[conte]])
 
 contes_bigrams = list(get_bigrams(tokens))
 contes_trigrams = list(get_trigrams(tokens))
 
 
-############### 5 : supprimer les mot d'arret
+
+"""  5 : supprimer les mot d'arret """
 
 mot_arret = stopwords.words('french')
 mot_arret.extend([ "a", "afin",
-                   "ah", "ai", "aie", "aient", "aies", "ait", "alors",
-                   "après", "as", "attendu", "au", "delà", "devant",
-                   "aucun", "aucune", "audit", "auprès", "auquel", "aura",
-                   "aurai", "auraient", "aurais", "aurait", "auras", "aurez",
-                   "auriez", "aurions", "aurons", "auront", "aussi", "autour",
+                   "ah", "ai", "aie", "aient", "aies", "ait", "alors", 'bon', 'peu' ,"veut"
+                   "après", "as", "attendu", "au", "delà", "devant", 'fois', 'encore',
+                   "aucun", "aucune", "audit", "auprès", "auquel", "aura", "vouloir"
+                   "aurai", "auraient", "aurais", "aurait", "auras", "aurez", "met", 'mettre'
+                   "auriez", "aurions", "aurons", "auront", "aussi", "autour", "tre",
                    "autre", "autres", "autrui", "aux", "auxdites", "auxdits",
                    "auxquelles", "auxquels", "avaient", "avais", "avait", "avant",
                    "avec", "avez", "aviez", "avions", "avons", "ayant", "ayez", "ayons",
                    "b", "bah", "banco", "ben", "bien", "bé", "c", "c'", "c'est", "c'était",
                    "l'","d'", "etant", "tel", "entre", "si", "ni", "etre", 'ete', 'apres' ,
                    'cela', 'ci','tous', 'trois', 'meme', 'etait','sais' , 'donc' , 'comme' ,
-                   'deja', 'assez' , 'depuis' , 'eh' , 'aupres', 'oui', 'non', 'cette' , 'oh'  ])
+                   'deja', 'assez' , 'depuis' , 'eh' , 'aupres', 'oui', 'non', 'cette' , 'oh' ,'tout' ])
 
 def filtre_motArret(contes) :
     for conte in contes :
@@ -115,7 +93,8 @@ def filtre_motArret(contes) :
 
 contes_filtrer = list(filtre_motArret(contes_trigrams))
 
-################ 6 : lemmatisation
+
+"""   6 : lemmatisation   """
 
 def lemmatiser(contes, permi=['ADV', 'ADJ','NOUN', 'VERB'] ) :
     for conte in contes :
@@ -123,12 +102,9 @@ def lemmatiser(contes, permi=['ADV', 'ADJ','NOUN', 'VERB'] ) :
 
 contes_lemmatiser = list(lemmatiser(contes_filtrer ))
 
-#print(contes_filtrer[5], len(contes_filtrer[5]))
-#print(contes_lemmatiser[5], len(contes_lemmatiser[5]))
 
 
-
-################7 :  cluster de mots pour la prédiction du sujet
+"""   7 :  cluster de mots pour la prédiction du sujet  """
 
 def join(contes):
     text = []
@@ -138,71 +114,57 @@ def join(contes):
         
 
 
-from sklearn.feature_extraction.text import CountVectorizer
-
 contes_lemmatiser_df = DataFrame( join(contes_lemmatiser),columns=['Contes'])
 contes_vectorizer = CountVectorizer(stop_words = mot_arret, ngram_range=(1, 4), min_df = 5, max_df = 0.8 )
-#transformer en matrice de nombre de token 
 contes = contes_vectorizer.fit_transform(contes_lemmatiser_df['Contes'])
 
-
-
-
-############## 8 : Recherche des meilleurs hyperparametre pour notre model
-
-
-
-from sklearn.decomposition import LatentDirichletAllocation as LDA
-from sklearn.model_selection import GridSearchCV
-
-
-parameters = [{'n_components': [ 5, 6,7,9,10, 15, 20, 25]}]
-# le grid search va nous dire lequelle est le meilleur nombre de type de sujet 
-              
+"""   8 : Recherche des meilleurs hyperparametre pour notre model  """
+""" le grid search va nous donnée le meilleur nombre de genre """
+         
 #model LDA non supervisé pour avoir proba des mots
 lda = LDA()
               
-# Grid Search 
+# Grid Search
+parameters = [{'n_components': [6,7,9,10,15,20,25]}]
 model = GridSearchCV(lda,parameters)
               
 #Fit the model
 model.fit(contes)
 best_lda = model.best_estimator_
 
-
-
+genre_num = []
+motClef = []
 def get_Genre(model , vectorisation , top_mot):
     tokens = vectorisation.get_feature_names()
     for ind, genre in enumerate(model.components_):
-        print("\nGenre {}:".format(ind))
-        print(" ".join([tokens[i] for i in genre.argsort()[:- top_mot - 1:-1]]))
+        genre_num.append("Genre {}:".format(ind))
+        motClef.append([tokens[i] for i in genre.argsort()[:- top_mot - 1:-1]])
+        #print("\nGenre {}:".format(ind))
+        #print(" ".join([tokens[i] for i in genre.argsort()[:- top_mot - 1:-1]]))
         
+get_Genre(best_lda, contes_vectorizer ,20)
 
+"""  9 : predire le sujet d'un nouveau texte   """
+""" probabilité d'apparition des mot dans chaque genre """
 
+genre_motClef = pd.DataFrame(motClef)
+#genre_motClef.columns = contes_vectorizer.get_feature_names()
+genre_motClef.index = genre_num
 
-get_Genre(best_lda, contes_vectorizer ,10)
+""""""""""""""""""""""""""""""""""""""""""""""""
 
+def predire_genre(texte, nlp = nlp ) :
+    '''préparer le texte'''
+    texte = sup_caractere_spéciaux(texte)
+    texte = list (tokenize(texte))
+    texte = list(get_trigrams(texte))
+    texte = list(filtre_motArret(texte))
+    texte = list(lemmatiser(texte))
+    texte = join(texte)
+    texte = contes_vectorizer.transform(texte)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ''' LDA Transform '''
+    genre_probability_scores = best_lda.transform(texte)
+    genre = genre_motClef.iloc[np.argmax(genre_probability_scores), :].values.tolist()
+    return  genre, genre_probability_scores
 
--- TaleBox_Interface.py
+++ TaleBox_Interface.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+import TaleBox as predicte
 
 class interface : 
     tale_box  =  Tk()
@@ -11,18 +12,16 @@ class interface :
     Light = '#F0F3F4'
     Beige = '#D2C0A7'
     Ivory = '#EEEDE7'
+    Tan = '#B68D40'
     
-
     def __init__(self):
         self.tale_box.title("TaleBox")
-        #box = Text(self.tale_box, bd=0, bg="white", height="8", width="50", font="Arial",)
 
     def setLogo(self):
         self.tale_box.iconbitmap("TaileBox_Icon.ico")
 
 
     def setColor(self,):
-
         self.tale_box.config(bg = self.Dark) 
 
 
@@ -32,40 +31,43 @@ class interface :
                  bd=0, bg=self.Beige,command= self.envoie )
         bouton.place(x=575, y=445, height=40)
 
-        #Insertion de repense 
+        #Entree de repense 
         self.reponse_entre = Text(self.tale_box, font=("Helvetica",13, 'normal','italic'))
         self.reponse_entre.place(x=20, y = 445 , height = 40, width = 500)
         self.reponse_entre.bind("<Return>", self.envoie)
 
-
-
-
-
-
     def tale_avance(self):
         self.box.config(bd=0,bg = self.Ivory, height="8", width="50", font="Arial")
         self.box.place(x = 20, y = 20, height = 400, width = 670)
+        
+        self.box.tag_add("tag1", "1.5", "1.9")
+        self.box.insert(END, "Bot : \n" ,"tag1")
+        self.box.insert(END, "Bonjours, entrez une histoire et je vous donne le genre.  \n\n" )
+        self.box.tag_config("tag1",font=("Verdana",14, 'bold'), foreground= self.Dark)
+        self.box.config(foreground= self.Dark, font=("Verdana", 10 ))
         self.box.config(state=DISABLED)
         
         
     def envoie(self) :
+        #Insertion de message et repense 
         msg = self.reponse_entre.get("1.0",'end-1c').strip()
         self.reponse_entre.delete("0.0",END)
+        self.box.tag_add("tag1", "1.5", "1.9")
+        self.box.config(state=NORMAL)
+
         if msg != '':
             self.box.config(state=NORMAL)
-            self.box.insert(END, " Vous: " + msg + '\n\n')
-            self.box.config(foreground= self.Dark, font=("Verdana", 12 )) 
-            #pred = Repondre_Prediction(msg)
+            self.box.insert(END, "Vous : \n" ,"tag1")
+            self.box.insert(END, ""+msg + '\n\n')
+            pred , proba= predicte.predire_genre(texte=msg)
             #reponce = getReponse(pred, intents)
-          
-            #self.box.insert(END, " Bot: " + reponce + '\n')           
-            #self.box.config(state=DISABLED)
+
+            self.box.insert(END, "Bot : \n" ,"tag1")
+            self.box.insert(END, "Cette histoire tourne autour du thème :\n" + ' '.join(pred) + '\n' )
+            self.box.config(state=DISABLED)
             self.box.yview(END)
 
         
-
-
-
     def affiche(self):
         self.tale_box.geometry("715x500")
         self.tale_box.maxsize(800,600)
@@ -85,7 +87,6 @@ class Repondre_Prediction:
     
     def __init__(self,message):
         self.message = message
-
 
 
 def main():
